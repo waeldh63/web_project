fc3749f753bc822d1d9f280c0e234ce9febf142a
reemhakim <104034639+reemhakim@users.noreply.github.com>
2022-04-30 01:02:54 +0300

    test test

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 
-const Cors = require("cors");
-const BodyParser = require("body-parser");
-
 const connectDB = require('./food-delivery/DB/Connection');
+
 const app = express();
 
 connectDB();
@@ -11,46 +9,4 @@ const Port = process.env.Port || 3000;
 
 app.listen(Port,()=>console.log('Server Started'));
 
-// test
-app.get("/search_restaurant", async (req, res) => {
-    try {
-        let results = await Collection.aggregate([
-
-{"$search":{
-
-"autocomplete":{
-
-"query":` ${req.query.term} `,
-
-"path":"name",
-
-"fuzzy":{
-
-    "maxEdits":2
-
-}
-}
-}
-
-}
-
-        ]).toArray();
-
-        res.send(results);
-
-       
-
-      res.send([]);
-
-    } catch (error) {
-
-      res.status(500).send({message: error.message})
-
-   
-
-    }
-
-  });
-
-app.listen(Port,()=>console.log('Server Started'));
 
