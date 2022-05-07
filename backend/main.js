3ae52b5b288acb63f3d8b20d20daec656812cf62
reemhakim <104034639+reemhakim@users.noreply.github.com>
2022-05-07 18:38:31 +0300

    autocomplete -backend

---
+++ backend/main.js
@@ -0,0 +1,43 @@
+const { MongoClient } = require("mongodb");
+const Express = require ("express");
+const Cors = require("cors");
+const BodyParser = require("body-parser");
+
+const client = new MongoClient("mongodb+srv://User:user12345@cluster0.fywre.mongodb.net/chunk?retryWrites=true&w=majority");
+const server = Express();
+
+server.use(BodyParser.json());
+server.use(BodyParser.urlencoded({extended:true}));
+server.use(Cors());
+
+var collection;
+
+server.get("/search", async (request, response) => {
+    try {
+        let result = await collection.aggregate([
+            {
+            "$search": {
+                "autocomplete": {
+                    "query": `${request.query.term}`,
+                    "path": "name",
+                    "fuzzy": {
+                        "maxEdits": 2
+                    }
+                }
+            }
+        }
+    ]).toArray();
+    response.send(result);
+    } catch(e) {
+        response.status(500).send({ message: e.message });
+    }
+})
+
+server.listen("3000", async () => {
+    try {
+        await client.connect();
+        collection = client.db("chunk").collection("recipes");
+    } catch (e) {
+        console.error(e);
+    }
+});
