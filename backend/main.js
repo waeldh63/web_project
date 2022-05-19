df83a443e39ce605821f972db5b5229c6ad0d39d
waeldh63 <waeldh63@gmail.com>
2022-05-19 12:22:03 +0300

    s

--- backend/main.js
+++ backend/main.js
@@ -2,6 +2,8 @@ const { MongoClient } = require("mongodb");
 const Express = require ("express");
 const Cors = require("cors");
 const BodyParser = require("body-parser");
+var express = require('express');
+var router = express.Router();
 
 const client = new MongoClient("mongodb+srv://User:user12345@cluster0.fywre.mongodb.net/chunk?retryWrites=true&w=majority");
 const server = Express();
@@ -42,8 +44,25 @@ server.listen("3001", async () => {
       
   // Fetching the records 
         
+
+  router.post('/italian', function (req, res, next) {
+	console.log("italian");
+    collection.find({ "cuisine": "Italian" }).toArray().then((ans) => {
+        for(i=0;i<ans.length;i++){
+                console.log(ans[i].name);
+                console.log("test");
+                //render ('italian.ejs', {"name":ans[i].name});
+                return res.render('italian.ejs', {"name":ans[i].name});
+    }
+});
+
+});
   collection.find({ "cuisine": "Italian" }).toArray().then((ans) => {
-    //console.log(ans);
+            for(i=0;i<ans.length;i++){
+                    console.log(ans[i].name);
+                    console.log("test");
+                    //render ('italian.ejs', {"name":ans[i].name});
+        }
     });
 
     collection.find({ "cuisine": "Mexican" }).toArray().then((ans) => {
